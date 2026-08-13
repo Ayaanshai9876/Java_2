a356dd7fb692f7a102886277deb4c73e374c4f42
Ayaanshai9876 <145261801+Ayaanshai9876@users.noreply.github.com>
2026-08-13 10:07:02 +0530

    Add files via upload

---
+++ Basics.java
@@ -0,0 +1,12 @@
+void main(){
+    System.out.println("Hello World" + " How are you"); // + between 2 strings means conactenation(means judna)
+    System.out.println(3); // it will print 3 and will go to next line same happened above
+    System.out.print(9); // it will print 9 and will stay in same line
+    System.out.println();
+    System.out.println("4+3");// stored a string will give out put 4+3
+    System.out.print(4+3); // will give addition of 2 nums
+    //"//" for single line comment '/*-*/' multiline comment
+
+    // main function is entry point of every code it will be executed first
+
+}
---
+++ Classical.java
@@ -0,0 +1,5 @@
+public class Classical {
+    public static void main(){
+        System.out.print("Hello wORLD");// CODE WILL EXECEUTE MAIN FUNCTION WHEN WE RUN IT
+    }
+}
